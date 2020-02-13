3c24a9a265ca7979cb5bdf4522ef943521df57f7
Igor Stelmashenko <thezaphod@gmail.com>
2020-02-13 23:26:59 +0000

    updated schema

--- esp32/motor/motor.ino
+++ esp32/motor/motor.ino
@@ -1,16 +1,17 @@
 /*
- * Copyright (c) 2018, circuits4you.com
- * All rights reserved.
- * 
- * ESP32 DAC - Digital To Analog Conversion Example
- */
+   Rui Santos
+   Complete Project Details http://randomnerdtutorials.com
+*/
 
-#define DAC1 25
-#define DAC2 26
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+#include <Update.h>
 
 // the number of the LED pin
-const int pPin = 16;  // 16 corresponds to GPIO16
-const int vPin = 17;  // 17 corresponds to GPIO17
+const int pPin = 17;  // 16 corresponds to GPIO16
+const int vPin = 16;  // 17 corresponds to GPIO17
 const int rPin = 5;   // 5  corresponds to GPIO5
 
 // setting PWM properties
@@ -18,48 +19,208 @@ const int freq = 5000;
 const int pChannel = 0;
 const int vChannel = 1;
 const int resolution = 8;
- 
-void setup() {
+
+
+const char* host = "esp32";
+const char* ssid = "t7rj";
+const char* password = "";
+
+//variabls to blink without delay:
+const int led = 2;
+unsigned long previousMillis = 0;        // will store last time LED was updated
+const long interval = 1000;           // interval at which to blink (milliseconds)
+int ledState = LOW;             // ledState used to set the LED
+
+WebServer server(80);
+
+/*
+   Login page
+*/
+
+const char* loginIndex =
+  "<form name='loginForm'>"
+  "<table width='20%' bgcolor='A09F9F' align='center'>"
+  "<tr>"
+  "<td colspan=2>"
+  "<center><font size=4><b>ESP32 Login Page</b></font></center>"
+  "<br>"
+  "</td>"
+  "<br>"
+  "<br>"
+  "</tr>"
+  "<td>Username:</td>"
+  "<td><input type='text' size=25 name='userid'><br></td>"
+  "</tr>"
+  "<br>"
+  "<br>"
+  "<tr>"
+  "<td>Password:</td>"
+  "<td><input type='Password' size=25 name='pwd'><br></td>"
+  "<br>"
+  "<br>"
+  "</tr>"
+  "<tr>"
+  "<td><input type='submit' onclick='check(this.form)' value='Login'></td>"
+  "</tr>"
+  "</table>"
+  "</form>"
+  "<script>"
+  "function check(form)"
+  "{"
+  "if(form.userid.value=='admin' && form.pwd.value=='admin')"
+  "{"
+  "window.open('/serverIndex')"
+  "}"
+  "else"
+  "{"
+  " alert('Error Password or Username')/*displays error message*/"
+  "}"
+  "}"
+  "</script>";
+
+/*
+   Server Index Page
+*/
+
+const char* serverIndex =
+  "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
+  "<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
+  "<input type='file' name='update'>"
+  "<input type='submit' value='Update'>"
+  "</form>"
+  "<div id='prg'>progress: 0%</div>"
+  "<script>"
+  "$('form').submit(function(e){"
+  "e.preventDefault();"
+  "var form = $('#upload_form')[0];"
+  "var data = new FormData(form);"
+  " $.ajax({"
+  "url: '/update',"
+  "type: 'POST',"
+  "data: data,"
+  "contentType: false,"
+  "processData:false,"
+  "xhr: function() {"
+  "var xhr = new window.XMLHttpRequest();"
+  "xhr.upload.addEventListener('progress', function(evt) {"
+  "if (evt.lengthComputable) {"
+  "var per = evt.loaded / evt.total;"
+  "$('#prg').html('progress: ' + Math.round(per*100) + '%');"
+  "}"
+  "}, false);"
+  "return xhr;"
+  "},"
+  "success:function(d, s) {"
+  "console.log('success!')"
+  "},"
+  "error: function (a, b, c) {"
+  "}"
+  "});"
+  "});"
+  "</script>";
+
+/*
+   setup function
+*/
+void setup(void) {
+
   Serial.begin(115200);
-  
-    // configure LED PWM functionalitites
+
+  // configure LED PWM functionalitites
   ledcSetup(pChannel, freq, resolution);
   ledcSetup(vChannel, freq, resolution);
-  
+
   // attach the channel to the GPIO to be controlled
   ledcAttachPin(pPin, pChannel);
   ledcAttachPin(vPin, vChannel);
 
   pinMode(rPin, OUTPUT);
+
+  // Connect to WiFi network
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  /*use mdns for host name resolution*/
+  if (!MDNS.begin(host)) { //http://esp32.local
+    Serial.println("Error setting up MDNS responder!");
+    while (1) {
+      delay(1000);
+    }
+  }
+  Serial.println("mDNS responder started");
+  /*return index page which is stored in serverIndex */
+  server.on("/", HTTP_GET, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/html", loginIndex);
+  });
+  server.on("/serverIndex", HTTP_GET, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/html", serverIndex);
+  });
+  /*handling uploading firmware file */
+  server.on("/update", HTTP_POST, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+    ESP.restart();
+  }, []() {
+    HTTPUpload& upload = server.upload();
+    if (upload.status == UPLOAD_FILE_START) {
+      Serial.printf("Update: %s\n", upload.filename.c_str());
+      if (!Update.begin(UPDATE_SIZE_UNKNOWN)) { //start with max available size
+        Update.printError(Serial);
+      }
+    } else if (upload.status == UPLOAD_FILE_WRITE) {
+      /* flashing firmware to ESP*/
+      if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+        Update.printError(Serial);
+      }
+    } else if (upload.status == UPLOAD_FILE_END) {
+      if (Update.end(true)) { //true to set the size to the current progress
+        Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+      } else {
+        Update.printError(Serial);
+      }
+    }
+  });
+  server.begin();
 }
 
-void loop() { 
+void loop(void) {
+  //Serial.println("server ");
+  //server.handleClient();
+  //delay(1);
 
-   while(true) {
-      digitalWrite(rPin, LOW);
-      delay(2000);  
-      digitalWrite(rPin, HIGH);
-      delay(2000);  
-   }    
-  
-  // increase the LED brightness
-  for(int dutyCycle = 180; dutyCycle <= 255; dutyCycle++){   
-    // changing the LED brightness with PWM
+  Serial.println("v motor ");
+  for (int dutyCycle = 255; dutyCycle >= 180; dutyCycle--) {
     ledcWrite(pChannel, dutyCycle);
     delay(30);
   }
+
+  ledcWrite(vChannel, 0 );
+  Serial.println("solenoid ");
+  digitalWrite(rPin, HIGH);
+  delay(2000);
+  digitalWrite(rPin, LOW);
+  delay(2000);
+
+  ledcWrite(vChannel, 255);
   
-
-  
-  // decrease the LED brightness
-  for(int dutyCycle = 255; dutyCycle >= 180; dutyCycle--){
-    // changing the LED brightness with PWM
-    ledcWrite(pChannel, dutyCycle);   
-    delay(15);
+//  Serial.println("p motor ");
+//  for (int dutyCycle = 100; dutyCycle <= 255; dutyCycle++) {
+//    ledcWrite(pChannel, dutyCycle);
+//    delay(20);
+//  }
 
 
-  }
- 
-//  int Value = 255; //255= 3.3V 128=1.65V 
-//  dacWrite(DAC1, 255);  
 }
