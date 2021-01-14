935239117bf42c6866ca6f7e3aae5222c5b4d3ea
Igor Stelmachenko <igor@igors-mac-mini.mynet>
2021-01-14 22:50:19 +0000

    interrupts and interrupted delay handling

--- esp32/motor/motor.ino
+++ esp32/motor/motor.ino
@@ -1,18 +1,15 @@
 /*
-   Rui Santos
-   Complete Project Details http://randomnerdtutorials.com
+ Motor tests
 */
-
-#include <WiFi.h>
-#include <WiFiClient.h>
-#include <WebServer.h>
-#include <ESPmDNS.h>
-#include <Update.h>
+#include <Adafruit_NeoPixel.h>
 
 // the number of the LED pin
 const int pPin = 17;  // 16 corresponds to GPIO16
 const int vPin = 16;  // 17 corresponds to GPIO17
 const int rPin = 5;   // 5  corresponds to GPIO5
+
+const int rgbPin = 23;  
+const int buttonPin = 19;
 
 // setting PWM properties
 const int freq = 5000;
@@ -20,10 +17,21 @@ const int pChannel = 0;
 const int vChannel = 1;
 const int resolution = 8;
 
+int mode = 0;
+unsigned long btPressedMillis = 0;
 
-const char* host = "esp32";
-const char* ssid = "t7rj";
-const char* password = "GZyVoTdPfrzio";
+// colors
+int b = 30; // brightnes 
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(1, rgbPin, NEO_GRB + NEO_KHZ800);
+
+uint32_t yellow = strip.Color(b,b,0);
+uint32_t green = strip.Color(0,b,0 );
+uint32_t cyan = strip.Color(0,b,b);
+uint32_t blue = strip.Color(0,0,b);
+uint32_t magenta = strip.Color(b,0,b);
+uint32_t red = strip.Color(b,0,0);
+
+uint32_t white = strip.Color(255,255,255);
 
 //variabls to blink without delay:
 const int led = 2;
@@ -31,100 +39,33 @@ unsigned long previousMillis = 0;        // will store last time LED was updated
 const long interval = 1000;           // interval at which to blink (milliseconds)
 int ledState = LOW;             // ledState used to set the LED
 
-WebServer server(80);
 
-/*
-   Login page
-*/
 
-const char* loginIndex =
-  "<form name='loginForm'>"
-  "<table width='20%' bgcolor='A09F9F' align='center'>"
-  "<tr>"
-  "<td colspan=2>"
-  "<center><font size=4><b>ESP32 Login Page</b></font></center>"
-  "<br>"
-  "</td>"
-  "<br>"
-  "<br>"
-  "</tr>"
-  "<td>Username:</td>"
-  "<td><input type='text' size=25 name='userid'><br></td>"
-  "</tr>"
-  "<br>"
-  "<br>"
-  "<tr>"
-  "<td>Password:</td>"
-  "<td><input type='Password' size=25 name='pwd'><br></td>"
-  "<br>"
-  "<br>"
-  "</tr>"
-  "<tr>"
-  "<td><input type='submit' onclick='check(this.form)' value='Login'></td>"
-  "</tr>"
-  "</table>"
-  "</form>"
-  "<script>"
-  "function check(form)"
-  "{"
-  "if(form.userid.value=='admin' && form.pwd.value=='admin')"
-  "{"
-  "window.open('/serverIndex')"
-  "}"
-  "else"
-  "{"
-  " alert('Error Password or Username')/*displays error message*/"
-  "}"
-  "}"
-  "</script>";
+const unsigned long debuncingDelay = 300;
 
-/*
-   Server Index Page
-*/
-
-const char* serverIndex =
-  "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>"
-  "<form method='POST' action='#' enctype='multipart/form-data' id='upload_form'>"
-  "<input type='file' name='update'>"
-  "<input type='submit' value='Update'>"
-  "</form>"
-  "<div id='prg'>progress: 0%</div>"
-  "<script>"
-  "$('form').submit(function(e){"
-  "e.preventDefault();"
-  "var form = $('#upload_form')[0];"
-  "var data = new FormData(form);"
-  " $.ajax({"
-  "url: '/update',"
-  "type: 'POST',"
-  "data: data,"
-  "contentType: false,"
-  "processData:false,"
-  "xhr: function() {"
-  "var xhr = new window.XMLHttpRequest();"
-  "xhr.upload.addEventListener('progress', function(evt) {"
-  "if (evt.lengthComputable) {"
-  "var per = evt.loaded / evt.total;"
-  "$('#prg').html('progress: ' + Math.round(per*100) + '%');"
-  "}"
-  "}, false);"
-  "return xhr;"
-  "},"
-  "success:function(d, s) {"
-  "console.log('success!')"
-  "},"
-  "error: function (a, b, c) {"
-  "}"
-  "});"
-  "});"
-  "</script>";
-
+void IRAM_ATTR isr() {
+  // debounce key
+  unsigned long currentMillis = millis();
+  if (currentMillis - btPressedMillis > debuncingDelay ) {
+    if(mode ==5) {
+      mode = 0;
+    } else {  
+      mode = mode + 1;
+    }    
+    btPressedMillis = currentMillis;  
+    Serial.println(mode); 
+  }
+  
+}
 /*
    setup function
 */
 void setup(void) {
 
   Serial.begin(115200);
+
+  strip.begin();
+  strip.show();
 
   // configure LED PWM functionalitites
   ledcSetup(pChannel, freq, resolution);
@@ -136,113 +77,179 @@ void setup(void) {
 
   pinMode(rPin, OUTPUT);
 
-  // Connect to WiFi network
-  //WiFi.begin(ssid, password);
-  Serial.println("");
-
-  // Wait for connection
-  //while (WiFi.status() != WL_CONNECTED) {
-  //  delay(500);
-  //  Serial.print(".");
-  //}
-//  Serial.println("");
-//  Serial.print("Connected to ");
-//  Serial.println(ssid);
-//  Serial.print("IP address: ");
-//  Serial.println(WiFi.localIP());
-//
-//  /*use mdns for host name resolution*/
-//  if (!MDNS.begin(host)) { //http://esp32.local
-//    Serial.println("Error setting up MDNS responder!");
-//    while (1) {
-//      delay(1000);
-//    }
-//  }
-//  Serial.println("mDNS responder started");
-//  /*return index page which is stored in serverIndex */
-//  server.on("/", HTTP_GET, []() {
-//    server.sendHeader("Connection", "close");
-//    server.send(200, "text/html", loginIndex);
-//  });
-//  server.on("/serverIndex", HTTP_GET, []() {
-//    server.sendHeader("Connection", "close");
-//    server.send(200, "text/html", serverIndex);
-//  });
-//  /*handling uploading firmware file */
-//  server.on("/update", HTTP_POST, []() {
-//    server.sendHeader("Connection", "close");
-//    server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
-//    ESP.restart();
-//  }, []() {
-//    HTTPUpload& upload = server.upload();
-//    if (upload.status == UPLOAD_FILE_START) {
-//      Serial.printf("Update: %s\n", upload.filename.c_str());
-//      if (!Update.begin(UPDATE_SIZE_UNKNOWN)) { //start with max available size
-//        Update.printError(Serial);
-//      }
-//    } else if (upload.status == UPLOAD_FILE_WRITE) {
-//      /* flashing firmware to ESP*/
-//      if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
-//        Update.printError(Serial);
-//      }
-//    } else if (upload.status == UPLOAD_FILE_END) {
-//      if (Update.end(true)) { //true to set the size to the current progress
-//        Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
-//      } else {
-//        Update.printError(Serial);
-//      }
-//    }
-//  });
-//  server.begin();
+  pinMode(buttonPin, INPUT_PULLUP);
+  attachInterrupt(buttonPin, isr, FALLING);
 }
 
-void loop(void) {
-  //Serial.println("server ");
-  //server.handleClient();
-  //delay(1);
+void displayMode() {
+    switch (mode) {
+      case 0:
+        strip.setPixelColor(0, yellow);
+        break;
+      case 1:
+         strip.setPixelColor(0, green);
+        break;     
+      case 2:
+         strip.setPixelColor(0, cyan);
+        break;
+      case 3:
+         strip.setPixelColor(0, blue);
+        break;        
+      case 4:
+         strip.setPixelColor(0, magenta);
+        break;       
+       case 5:
+         strip.setPixelColor(0, red);
+        break;  
+    }
+    strip.show(); 
+}
 
+
+
+
+
+
+
+void loop(void) {
+
+      switch (mode) {
+      case 0:
+          program0();
+        break;
+      case 1:
+           program1();
+        break;     
+      case 2:
+           program2();
+        break;
+      case 3:
+           program3();
+        break;        
+      case 4:
+           program4();
+        break;       
+       case 5:
+           program2();
+        break;  
+    }
+}
+
+
+int delay_inter(int delaytime, int current_mode) {
+  if (current_mode != mode) {
+    return 0;
+  } else {
+    delay(delaytime);
+  }
+  return 1;
+}
+
+void program3(void) {
+  Serial.println("program3");
+  int current_mode = mode;
+  displayMode();
   ledcWrite(pChannel, 0);
   for (int i = 0; i < 20; i++) {
     ledcWrite(vChannel, 255);
-    delay(random(500,1000));
-    ledcWrite(vChannel, random(150, 200));
-    delay(random(500,1000));
+    if (delay_inter(300, current_mode) == 0) { goto bailout; }
+    ledcWrite(vChannel, 0);
+    if (delay_inter(300, current_mode) == 0) { goto bailout; }
   }
+  bailout:
+  return;
+}
 
+void program4(void) {
+  Serial.println("program4");
+  int current_mode = mode;
+  displayMode();
+  ledcWrite(pChannel, 0);
   for (int i = 0; i < 20; i++) {
+    ledcWrite(vChannel, 255);
+    if (delay_inter(150, current_mode) == 0) { goto bailout; }
+    ledcWrite(vChannel, 0);
+    if (delay_inter(150, current_mode) == 0) { goto bailout; }
+  }
+  for (int i = 0; i < 12; i++) {
+    ledcWrite(vChannel, 180);
+    if (delay_inter(150, current_mode) == 0) { goto bailout; }
+    ledcWrite(vChannel,0);
+    if (delay_inter(150, current_mode) == 0) { goto bailout; }
+  }
+  bailout:
+  return;
+}
+
+
+void program2(void) {
+  Serial.println("program2");
+  int current_mode = mode;
+  displayMode();
+  for (int i = 0; i < 20; i++) {
+    ledcWrite(vChannel, 0);
+    ledcWrite(pChannel, 255);
+    if (delay_inter(1000, current_mode) == 0) { goto bailout; }
+    if (delay_inter(1000, current_mode) == 0) { goto bailout; }
+    if (delay_inter(1000, current_mode) == 0) { goto bailout; }
     ledcWrite(vChannel, 200);
-    ledcWrite(pChannel, 160);
-    delay(700);
+    if (delay_inter(1000, current_mode) == 0) { goto bailout; }
+    if (delay_inter(1000, current_mode) == 0) { goto bailout; }
+    ledcWrite(vChannel, 0);
+    if (delay_inter(1000, current_mode) == 0) { goto bailout; }
+
+    
     digitalWrite(rPin, HIGH);
     ledcWrite(pChannel, 0);
-    delay(700);
+    if (delay_inter(200, current_mode) == 0) { goto bailout; }
     digitalWrite(rPin, LOW);
     ledcWrite(vChannel, 200);
-    
-    delay(random(100));
+
+    if (delay_inter(random(100), current_mode) == 0) { goto bailout; }
   }
+  bailout:
+  return;
 }
 
 void program1(void) {
-
+  Serial.println("program1");
+  int current_mode = mode;
+  displayMode();
   ledcWrite(pChannel, 0);
   for (int i = 0; i < 20; i++) {
+    ledcWrite(vChannel, 130);
+    if (delay_inter(random(500, 1000), current_mode) == 0) { goto bailout; }
     ledcWrite(vChannel, 255);
-    delay(random(500,1000));
-    ledcWrite(vChannel, random(0,150));
-    delay(random(500,1000));
+    if (delay_inter(random(100, 800), current_mode) == 0) { goto bailout; }
+  }
+  bailout:
+  return;
+}
+
+void program0(void) {
+  Serial.println("program0");
+  int current_mode = mode;
+  displayMode();
+  ledcWrite(pChannel, 0);
+  for (int i = 0; i < 20; i++) {
+    ledcWrite(vChannel, 130);
+    if (delay_inter(random(500, 1000), current_mode) == 0) { goto bailout; }
+    ledcWrite(vChannel, 255);
+    if (delay_inter(random(100, 800), current_mode) == 0) { goto bailout; }
   }
 
-  for (int i = 0; i < 10; i++) {
-    ledcWrite(vChannel, random(0,150));
-    ledcWrite(pChannel, 160);
-    delay(random(300,1200));
+  for (int i = 0; i < 20; i++) {
+    ledcWrite(vChannel, 200);
+    ledcWrite(pChannel, 255);
+    if (delay_inter(2000, current_mode) == 0) { goto bailout; }
     digitalWrite(rPin, HIGH);
     ledcWrite(pChannel, 0);
-    delay(700);
+    if (delay_inter(100, current_mode) == 0) { goto bailout; }
     digitalWrite(rPin, LOW);
     ledcWrite(vChannel, 200);
-    
-    delay(random(500,1000));
+
+    if (delay_inter(random(100), current_mode) == 0) { goto bailout; }
   }
+  bailout:
+  return;
 }
